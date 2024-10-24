0ffad71802b58bcbba2db8d28cc16fcc99bfd46c
Suresh Gadisetti <suresh.gadisetti@care.ai>
2024-10-24 17:23:00 +0530

    Material UI Box

---
+++ MUI/src/App.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const App = () => {
+  return (
+    <Box sx={{color:'red',border:'1px solid red'}}>
+      This is sample
+    </Box>
+  )
+}
+
+export default App
